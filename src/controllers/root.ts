63a8c283a38874724d412f3437b2c79ed32a5af3
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-04-27 18:29:44 -0700

    fix awaiting on tx

--- src/controllers/root.ts
+++ src/controllers/root.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Flow, Get, Put } from 'amala'
+import { Body, Controller, Flow, Get, Post } from 'amala'
 import { utils } from 'ethers'
 import AddressBody from '@/validators/AddressBody'
 import authenticate from '@/middlewares/authenticate'
@@ -6,15 +6,16 @@ import streetCredLedger from '@/helpers/streetCredLedger'
 
 @Controller('/')
 export default class RootController {
-  @Put('/add-roots')
+  @Post('/add-roots')
   @Flow(authenticate)
   async addRoots(@Body({ required: true }) { addresses }: AddressBody) {
-    await streetCredLedger.addRoots(
+    const tx = await streetCredLedger.addRoots(
       addresses.map((tokenAddress) => ({
         tokenAddress,
         merkleRoot: utils.hexZeroPad('0x0', 32),
       }))
     )
+    await tx.wait()
   }
 
   @Get('/contract-address')
