0e6613dd716279262f776f9c8335919b038f0861
sarmat <skhachirov12@gmail.com>
2022-05-06 17:08:10 +0300

    minor fix

--- src/helpers/verifyContract.ts
+++ src/helpers/verifyContract.ts
@@ -1,15 +1,28 @@
-import {
-  EtherscanVerifyResponse,
-  toVerifyRequest,
-} from '@/etherscan/EtherscanVerifyContractRequest'
-import etherscanClient from '@/helpers/client'
+import * as SCERC721Derivative from '@/models/SCERC721Derivative.json'
+import client from '@/helpers/client'
+import env from '@/helpers/env'
 
-export default function verifyContract(
+const verifyContract = (
   contractAddress: string,
   constructorArguments: string
-): Promise<EtherscanVerifyResponse> {
-  const request = toVerifyRequest(contractAddress, constructorArguments)
+) => {
+  const request = {
+    apikey: env.ETHERSCAN_API_KEY,
+    module: 'contract',
+    action: 'verifysourcecode',
+    contractaddress: contractAddress.toString(),
+    codeformat: 'solidity-standard-json-input',
+    contractname: 'contracts/SCERC721Derivative.sol:SCERC721Derivative',
+    compilerversion: 'v0.8.4+commit.c7e474f2',
+    constructorArguements: constructorArguments,
+    sourceCode: JSON.stringify(SCERC721Derivative),
+  }
   const parameters = new URLSearchParams({ ...request })
-
-  return etherscanClient.post('', parameters)
+  return client.post<{
+    status: string
+    message: string
+    result: string
+  }>('', parameters)
 }
+
+export default verifyContract
